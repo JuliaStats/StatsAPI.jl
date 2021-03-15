e3e696aa723d177cd336384a862c0cce9bd64af7
Milan Bouchet-Valat <nalimilan@club.fr>
2021-03-15 09:34:42 +0100

    Add pairwise (#1)

--- src/StatsAPI.jl
+++ src/StatsAPI.jl
@@ -1,3 +1,21 @@
 module StatsAPI
 
+#    pairwise(f, x[, y])
+#
+# Return a matrix holding the result of applying `f` to all possible pairs
+# of entries in iterators `x` and `y`, and return it.
+#
+# This generic function is owned by StatsBase.jl, which is the sole provider
+# of the default definition.
+function pairwise end
+
+#    pairwise!(f, dest::AbstractMatrix, x[, y])
+#
+# Store in matrix `dest` the result of applying `f` to all possible pairs
+# of entries in iterators `x` and `y`, and return it.
+#
+# This generic function is owned by StatsBase.jl, which is the sole provider
+# of the default definition.
+function pairwise! end
+
 end # module
