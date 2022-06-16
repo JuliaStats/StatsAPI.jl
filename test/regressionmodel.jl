11505ff462311b123b7266d6dcdf598b7978cb5a
Giuseppe Ragusa <giuseppe.ragusa@gmail.com>
2022-06-17 00:38:46 +0200

    Change type name for clarity

--- test/regressionmodel.jl
+++ test/regressionmodel.jl
@@ -6,22 +6,22 @@ using StatsAPI: RegressionModel, crossmodelmatrix
 struct MyRegressionModel <: RegressionModel
 end
 
-struct ItsRegressionModel <: RegressionModel
-    wts
+struct MyWeightedRegressionModel <: RegressionModel
+    wts::AbstractVector
 end
 
 StatsAPI.modelmatrix(::MyRegressionModel) = [1 2; 3 4]
 
-function StatsAPI.modelmatrix(r::ItsRegressionModel; weighted::Bool=false)  
+function StatsAPI.modelmatrix(r::MyWeightedRegressionModel; weighted::Bool=false)
     X = [1 2; 3 4]
-    weighted ? sqrt.(r.wts).*X : X 
+    weighted ? sqrt.(r.wts).*X : X
 end
 
 w = [0.3, 0.2]
 
 @testset "TestRegressionModel" begin
     m = MyRegressionModel()
-    r = ItsRegressionModel(w)
+    r = MyWeightedRegressionModel(w)
 
     @test crossmodelmatrix(m) == [10 14; 14 20]
     @test crossmodelmatrix(m; weighted=false) == [10 14; 14 20]
@@ -31,7 +31,6 @@ w = [0.3, 0.2]
     @test crossmodelmatrix(r; weighted=false) == [10 14; 14 20]
     @test crossmodelmatrix(r; weighted=true) ≈ [2.1 3.0; 3.0 4.4]
     @test crossmodelmatrix(r; weighted=true) isa Symmetric
-
 end
 
 end # module TestRegressionModel
