520162c06c2a1de2dc8adb24db282c48e27e67c0
Alex Arslan <ararslan@comcast.net>
2022-07-30 10:43:46 -0700

    Add `linearpredictor` and `linearpredictor!`

--- src/regressionmodel.jl
+++ src/regressionmodel.jl
@@ -117,3 +117,18 @@ Return the offset used in the model, i.e. the term added to the linear predictor
 known coefficient 1.
 """
 function offset end
+
+"""
+    linearpredictor(model::RegressionModel)
+
+Return the model's linear predictor, `Xβ` where `X` is the model matrix and `β` is the
+vector of coefficients, or `Xβ + offset` if the model was fit with an offset.
+"""
+function linearpredictor end
+
+"""
+    linearpredictor!(storage, model::RegressionModel)
+
+In-place version of [`linearpredictor`](@ref), storing the result in `storage`.
+"""
+function linearpredictor! end
