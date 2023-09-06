9845ac3b266a5c76d009f65882d85c8770fde58b
Matthieu Gomez <gomez.matthieu@gmail.com>
2023-09-06 08:45:51 -0400

    Update regressionmodel.jl

--- test/regressionmodel.jl
+++ test/regressionmodel.jl
@@ -10,9 +10,10 @@ StatsAPI.modelmatrix(::MyRegressionModel) = [1 2; 3 4]
 
 @testset "TestRegressionModel" begin
     m = MyRegressionModel()
-
+    @test !hasintercept(m) 
     @test crossmodelmatrix(m) == [10 14; 14 20]
     @test crossmodelmatrix(m) isa Symmetric
+    
 end
 
 end # module TestRegressionModel
