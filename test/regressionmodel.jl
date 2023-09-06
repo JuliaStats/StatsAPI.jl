29ac7cce914cf6e5f6fc3d36870f99f72346c201
Phillip Alday <phillip.alday@beacon.bio>
2023-09-06 22:34:07 +0200

    missed one

--- test/regressionmodel.jl
+++ test/regressionmodel.jl
@@ -1,26 +1,12 @@
 module TestRegressionModel
 
 using Test, LinearAlgebra, StatsAPI
-using StatsAPI: RegressionModel, crossmodelmatrix, vif
+using StatsAPI: RegressionModel, crossmodelmatrix
 
 struct MyRegressionModel <: RegressionModel
 end
 
 StatsAPI.modelmatrix(::MyRegressionModel) = [1 2; 3 4]
-StatsAPI.vcov(::MyRegressionModel) = [1 0; 0 1]
-
-struct MyRegressionModel2 <: RegressionModel
-end
-
-StatsAPI.modelmatrix(::MyRegressionModel2) = [1 2; 1 2]
-StatsAPI.vcov(::MyRegressionModel2) = [1 0; 0 1]
-
-struct MyRegressionModel3 <: RegressionModel
-end
-
-StatsAPI.modelmatrix(::MyRegressionModel3) = [1 2 3; 1 2 3]
-StatsAPI.vcov(::MyRegressionModel3) = [1 0 0; 0 1 0; 0 0 1]
-
 
 @testset "TestRegressionModel" begin
     m = MyRegressionModel()
