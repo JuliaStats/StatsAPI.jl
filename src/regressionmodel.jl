be9e1ae885e811d30bf3551bb1d1b53efbbbe1cb
Phillip Alday <palday@users.noreply.github.com>
2023-09-06 08:06:20 -0500

    Update src/regressionmodel.jl

--- src/regressionmodel.jl
+++ src/regressionmodel.jl
@@ -146,7 +146,7 @@ The [VIF](https://en.wikipedia.org/wiki/Variance_inflation_factor) measures
 the increase in the variance of a parameter's estimate in a model with multiple parameters relative to
 the variance of a parameter's estimate in a model containing only that parameter.
 
-See also [`coefnames`](@ref), [`gvif`](@ref).
+See also [`coefnames`](@ref).
 
 !!! warning
     This method will fail if there is (numerically) perfect multicollinearity,
