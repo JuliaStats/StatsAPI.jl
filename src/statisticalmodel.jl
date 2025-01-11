6e73c07a3f100444e5eb70d0799fc4ccac0eecd6
Alex Wadell <awadell@gmail.com>
2025-01-11 11:55:19 -0500

    fix typo in adjr2

--- src/statisticalmodel.jl
+++ src/statisticalmodel.jl
@@ -290,7 +290,7 @@ adjr2(model::StatisticalModel)
 
 Adjusted pseudo-coefficient of determination (adjusted pseudo R-squared).
 For nonlinear models, one of the several pseudo R² definitions must be chosen via `variant`.
-The only currently supported variants are `:MacFadden`, defined as ``1 - (\\log (L) - k)/\\log (L0)`` and
+The only currently supported variants are `:McFadden`, defined as ``1 - (\\log (L) - k)/\\log (L0)`` and
 `:devianceratio`, defined as ``1 - (D/(n-k))/(D_0/(n-1))``.
 In these formulas, ``L`` is the likelihood of the model, ``L0`` that of the null model
 (the model including only the intercept), ``D`` is the deviance of the model,
