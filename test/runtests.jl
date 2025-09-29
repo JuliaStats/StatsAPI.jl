0a903f36614561d1fc8a48b04b644dd4e7c427cc
David Müller-Widmann <devmotion@users.noreply.github.com>
2025-09-29 15:30:11 +0200

    Declare StatsAPI functionality as `public` on Julia >= 1.11

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,6 +2,25 @@ using Test, StatsAPI
 
 @testset "StatsAPI" begin
 
+# Check that all names in StatsAPI are public
+if isdefined(Base, :ispublic)
+    names_statsapi = names(StatsAPI; all = true)
+    filter!(names_statsapi) do name
+        if name == :eval || name == :include
+            # Ignore `eval` and `include` (available in every `module`)
+            return false
+        elseif startswith(string(name), "#")
+            # Ignore names of the form `#xyz...`
+            return false
+        else
+            return true
+        end
+    end
+    @testset "public: $(name)" for name in names_statsapi
+        @test Base.ispublic(StatsAPI, name)
+    end
+end
+
 include("regressionmodel.jl")
 include("statisticalmodel.jl")
 
